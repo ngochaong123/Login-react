65ff8d91ad59090dc45791c86e3cfde5d1f37b22
Cong Kha <hoangcongkha96@gmail.com>
2023-12-03 18:59:00 +0700

    change

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,16 +1,21 @@
 import React, { useState } from 'react';
-import "./App.css"
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import "./Components/script.js";
+import "./App.css"
+
 
 function App() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const navigate = useNavigate();
 
   const handleLogin = () => {
     axios.post('http://localhost:5000/login', { username, password })
       .then(response => {
         console.log(response.data);
         // Handle successful login
+        navigate('/Chatbot');
       })
       .catch(error => {
         console.error(error.response.data);
@@ -19,22 +24,53 @@ function App() {
   };
 
   return (
-    <div className='login-container'>
-
-      <fornt className="fornt-login">
-        <h1 style={{textAlign:"center",padding:"5px",color:"white"}}>Login</h1>
-        <div className='space'>
-           <label className='name-login'>Username </label>
-          <input className='input' type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+    <div class="container" id="container">
+        <div class="form-container sign-up">
+            <form>
+                <h1>Create Account</h1>
+                <div class="social-icons">
+                    <a class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                    <a class="icon"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a class="icon"><i class="fa-brands fa-github"></i></a>
+                    <a class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+                <span>or use your email for registeration</span>
+                <input type="text" placeholder="Name" />
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Password" />
+                <button>Sign Up</button>
+            </form>
         </div>
-        <div className='space'>
-          <label className='name-login'>Password </label>
-          <input className='input' type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <div class="form-container sign-in">
+            <form>
+                <h1>Sign In</h1>
+                <div class="social-icons">
+                    <a class="icon"><i class="fa-brands fa-google-plus-g"></i></a>
+                    <a class="icon"><i class="fa-brands fa-facebook-f"></i></a>
+                    <a class="icon"><i class="fa-brands fa-github"></i></a>
+                    <a class="icon"><i class="fa-brands fa-linkedin-in"></i></a>
+                </div>
+                <span>or use your email password</span>
+                <input type="email" placeholder="Email"  value={username} onChange={(e) => setUsername(e.target.value)} />
+                <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                <a href="#">Forget Your Password?</a>
+                <button onClick={handleLogin} className='buttton'>Sign In</button>
+            </form>
         </div>
-        
-        <button onClick={handleLogin} className='buttton'>Login</button>
-      </fornt>
-
+        <div class="toggle-container">
+            <div class="toggle">
+                <div class="toggle-panel toggle-left">
+                    <h1>Welcome Back!</h1>
+                    <p>Enter your personal details to use all of site features</p>
+                    <button class="hidden" id="login">Sign In</button>
+                </div>
+                <div class="toggle-panel toggle-right">
+                    <h1>Hello, Friend!</h1>
+                    <p>Register with your personal details to use all of site features</p>
+                    <button class="hidden" id="register">Sign Up</button>
+                </div>
+            </div>
+        </div>
     </div>
   );
 }
